443bf8ade9f57aa6f0ec0482f7d6020b9a175d21
TamikoSRN <thomasereno1@gmail.com>
2022-06-01 16:24:46 -0300

    Items are displayed now

---
+++ src/app/components/items/items.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Item } from '../../Models/item';
+
+@Component({
+  selector: 'app-items',
+  templateUrl: './items.component.html',
+  styleUrls: ['./items.component.css']
+})
+export class ItemsComponent implements OnInit {
+
+  items: Item[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.items = [
+      {
+        id: 0,
+        title: 'Apple',
+        price: 50,
+        quantity: 2,
+        completed: false,
+      },
+      {
+        id: 1,
+        title: 'Bread',
+        price: 35,
+        quantity: 5,
+        completed: true,
+      },
+    ]
+  }
+
+}
